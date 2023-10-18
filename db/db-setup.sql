0d59b41d30160c49636f2f353a1bc4a46c52c212
Muno Siyakurima <siyakurimam@carleton.edu>
2023-10-17 23:08:04 -0500

    Some additions to database

--- db/db-setup.sql
+++ db/db-setup.sql
@@ -2,7 +2,21 @@ CREATE DATABASE MasterMinds;
 
 USE MasterMinds;
 
-CREATE TABLE Players(
-	first TEXT,
-	last TEXT
+CREATE TABLE PlayerData(
+	name TEXT,
+	gameID int AUTO_INCREMENT,
+	moves TEXT,
+	attempts int,
+	gameComplete boolean,
+	PRIMARY KEY (gameID)
 );
+
+
+INSERT INTO PlayerData (name, moves, attempts, gameComplete) VALUES ('Muno', 'mmmm', '3', 0);
+
+
+
+CREATE USER 'webapp'@'%' IDENTIFIED BY 'masterminds1';
+
+GRANT ALL ON MasterMinds.* TO 'webapp'@'%';
+
