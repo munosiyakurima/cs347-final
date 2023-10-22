db9303e465a041be55f76bc0b0f85ba7804fd2a1
Barinamene Nwike <113485330+BNwike47@users.noreply.github.com>
2023-10-22 14:17:23 -0500

    Smoothened the gameplay experience
    Integrated a text-based board.  Added some prompts to indicate how the user did.  Has a recursive game loop, that should function, but needs to be tested

--- app/game_logic.py
+++ app/game_logic.py
@@ -1,10 +1,13 @@
 import random
+import game_board
 
 COLOR_MASTER = ['red', 'blue', 'black', 'white', 'brown', 'orange', 'purple', 'pink']
 
 max_password_len = 4
 max_password_attempts = 4
 allow_repeats = False
+attempts = 0
+board = game_board.GameBoard(max_password_len, max_password_attempts)
 
 rules = []
 
@@ -21,21 +24,21 @@ def password_generator():
 
     
     return password
-    
-def valid_moves(user_guess):
+
+#Makes sure user's guess is the right length and has viable colors    
+def valid_moves(user_guess: str):
     if len(user_guess) != max_password_len:
         return False
-    if user_guess not in COLOR_MASTER:
+    if user_guess.lower not in COLOR_MASTER:
         return False
     return True
 
+#Parses the user's guess to see what they got correct. Prints out relevant information and returns false if there are any incorrect guesses
 def guess_checker(user_guess, password):
     guess = user_guess.split()
-    hint = ""
     correct = 0
     false_position = 0
     empty = 0
-    
     for i in range(max_password_len):
         if(guess[i] == password[i]):
             correct += 1
@@ -44,10 +47,56 @@ def guess_checker(user_guess, password):
                 false_position += 1
             else:
                 empty += 1
-    hint = correct + false_position + empty
-    print("There are ", correct, " correct in the right position")
-    print("There are ", false_position, " correct in the wrong position ")
-    return hint
+    
+    if(correct == max_password_len):
+        return True
+    print("There are " + str(correct) + " correct in the right position")
+    print("There are " + str(false_position) + " correct in the wrong position ")
+    print()
+    return False
+
+#A recursive function that playes through the entirety of the game Mastermind
+def game_loop():
+    password = password_generator()
+    board.display_board()
+
+#REMEMBER TO TAKE OUT THE PASSWORD!!!!
+    print(password)
+    user_guess = input("Enter your guess: ")
+
+    #repeats prompt until your input is valid
+    while not valid_moves(user_guess):
+        print("Your input was wrong. Care to try again?")
+        print
+        user_guess = input("Enter your guess: ")
+
+    #update the text board, and display
+    board.update_board(attempts, user_guess)
+    board.display_board()
+
+    #If the user's guess is correct, end the game
+    if(guess_checker(user_guess, password)):
+        finished = True
+        print("Code Broken!  You win!!")
+        print
+        print
+        return
+    
+    #If the user made too many attempts, end the game
+    elif(attempts>= max_password_attempts):
+        print("Too Bad!  This game is over")
+        print("The correct password was: " + password)
+        print
+        print("Thank you for playing!")
+        return
+    
+    #if the user's guess is wrong, we move to the next line
+    else:
+        print("Not quite.  Let's try again")
+        attempts += 1
+        game_loop()
+        
+    return
                 
 
 
