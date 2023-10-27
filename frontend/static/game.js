f639b590f6a9f477498a7048dea7f1e2bb068526
Pey-Bass <kingdavidpds@gmail.com>
2023-10-27 02:29:12 -0500

    updated game css

--- frontend/static/game.js
+++ frontend/static/game.js
@@ -1,27 +1,27 @@
 function setGameColors(colors) {
-    let colortoggles = document.getElementsByClassName("colortoggle");
-    for (let i=0; i < colortoggles.length; i++) {
-        colortoggles[i].style.backgroundcolor = colors[i];
+    let toggles = document.getElementsByClassName("toggle");
+    for (let i=0; i < toggles.length; i++) {
+        toggles[i].style.backgroundcolor = colors[i];
     }
 }
 
 function toggleUnmarked(color){
     var element = document.getElementById(color);
-    element.classList.toggle("unmarked");
+    element.classList.toggle("marked");
 }
 
-(function checkOptions() {
-    let colortoggles = document.getElementsByClassName("colortoggle");
-    let cur_unmarked = document.getElementsByClassName("unmarked");
-    if (cur_unmarked.length == 2) {
-        for (let i=0; i < colortoggles.length; i++) {
-            if (colortoggles[i].classList.contains("unmarked")) {
-                colortoggles[i].disabled = true;
+function checkOptions() {
+    let toggles = document.getElementsByClassName("toggle");
+    let cur_unmarked = document.getElementsByClassName("marked");
+    if (cur_unmarked.length == 4) {
+        for (let i=0; i < toggles.length; i++) {
+            if (!toggles[i].classList.contains("marked")) {
+                toggles[i].disabled = true;
             }
         } 
     } else {
-        for (let i=0; i < colortoggles.length; i++) {
-            colortoggles[i].disabled = false;
+        for (let i=0; i < toggles.length; i++) {
+            toggles[i].disabled = false;
         }
     }
-})();
+}
