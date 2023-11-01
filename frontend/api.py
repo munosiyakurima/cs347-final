1012f3a58a54f488c950329e441b376eb146c67e
Muno Siyakurima <siyakurimam@carleton.edu>
2023-11-01 00:29:26 -0500

    Changes to the database

--- frontend/api.py
+++ frontend/api.py
@@ -6,7 +6,7 @@ import random
 import mysql.connector
 
 app = Flask(__name__, 
-        static_url_path='',
+        static_url_path='/static',
         static_folder='static',
         template_folder='templates')
 
